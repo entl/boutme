425f19bc7b77240ec0a67e18e7d6d7a6ab43dc64
entl <maxvorobyov01@gmail.com>
2024-04-03 21:43:00 +0100

    finished service for project

--- internal/service/project_service.go
+++ internal/service/project_service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/entl/boutme/internal/database"
 	"github.com/entl/boutme/internal/models"
 	"github.com/entl/boutme/internal/repository"
+	"github.com/google/uuid"
 	"github.com/mitchellh/mapstructure"
 )
 
@@ -61,4 +62,68 @@ func (s *ProjectService) GetProjects(ctx context.Context) ([]models.ProjectRespo
 	}
 
 	return projectResponse, nil
+}
+
+func (s *ProjectService) GetProject(ctx context.Context, id string) (models.ProjectResponseDTO, error) {
+	projectDb, err := s.projectRepository.GetProject(ctx, id)
+	if err != nil {
+		fmt.Println("error getting project service: ", err)
+		return models.ProjectResponseDTO{}, err
+	}
+
+	var projectResponse models.ProjectResponseDTO
+	err = mapstructure.Decode(projectDb, &projectResponse)
+	if err != nil {
+		fmt.Println("error decoding project: ", err)
+		return models.ProjectResponseDTO{}, err
+	}
+
+	return projectResponse, nil
+}
+
+func (s *ProjectService) UpdateProject(ctx context.Context, id string, project models.ProjectUpdateDTO) (models.ProjectResponseDTO, error) {
+	var projectParams database.UpdateProjectParams
+	err := mapstructure.Decode(project, &projectParams)
+	if err != nil {
+		fmt.Println("error decoding project: ", err)
+		return models.ProjectResponseDTO{}, errors.New(fmt.Sprintf("error decoding project: %v", err))
+	}
+
+	projectParams.ID, err = uuid.Parse(id)
+	if err != nil {
+		fmt.Println("error parsing project id: ", err)
+		return models.ProjectResponseDTO{}, errors.New(fmt.Sprintf("error parsing project id: %v", err))
+
+	}
+
+	projectDb, err := s.projectRepository.UpdateProject(ctx, projectParams)
+	if err != nil {
+		fmt.Println("error updating project service: ", err)
+		return models.ProjectResponseDTO{}, err
+	}
+
+	var projectResponse models.ProjectResponseDTO
+	err = mapstructure.Decode(projectDb, &projectResponse)
+	if err != nil {
+		fmt.Println("error decoding project: ", err)
+		return models.ProjectResponseDTO{}, err
+	}
+
+	return projectResponse, nil
+}
+
+func (s *ProjectService) DeleteProject(ctx context.Context, id string) error {
+	projectId, err := uuid.Parse(id)
+	if err != nil {
+		fmt.Println("error parsing project id: ", err)
+		return errors.New(fmt.Sprintf("error parsing project id: %v", err))
+	}
+
+	err = s.projectRepository.DeleteProject(ctx, projectId)
+	if err != nil {
+		fmt.Println("error deleting project service: ", err)
+		return err
+	}
+
+	return nil
 }
