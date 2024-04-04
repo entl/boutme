95b87dc4c02cad73cf7a9aac3ac4e97006e8ea03
entl <maxvorobyov01@gmail.com>
2024-04-04 23:46:00 +0100

    fix user table

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,2 +1,8 @@
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1;
+
+-- name: AddUser :one
+INSERT INTO users (id, username, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *;
+
+-- name: CountUsers :one
+SELECT COUNT(*) FROM users;
--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
+    id uuid PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
