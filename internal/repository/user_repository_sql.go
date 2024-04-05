116dcc1728e1201f910b2bbbb45583749c1545fb
entl <maxvorobyov01@gmail.com>
2024-04-05 20:56:00 +0100

    add user repo

--- internal/repository/user_repository_sql.go
+++ internal/repository/user_repository_sql.go
@@ -2,18 +2,44 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/entl/boutme/internal/database"
 )
 
 type SqlUserRepository struct {
-	db  *database.Queries
-	ctx context.Context
+	db *database.Queries
 }
 
 func NewSqlUserRepository(db *database.Queries) *SqlUserRepository {
 	return &SqlUserRepository{db: db}
 }
 
-func (repository *SqlUserRepository) GetUser(username string) (database.User, error) {
-	return repository.db.GetUser(repository.ctx, username)
+func (repository *SqlUserRepository) GetUser(ctx context.Context, username string) (database.User, error) {
+	user, err := repository.db.GetUser(ctx, username)
+	if err != nil {
+		fmt.Println("error getting user rep: ", err)
+		return database.User{}, err
+	}
+
+	return user, nil
+}
+
+func (repository *SqlUserRepository) AddUser(ctx context.Context, userParams database.AddUserParams) (database.User, error) {
+	user, err := repository.db.AddUser(ctx, userParams)
+	if err != nil {
+		fmt.Println("error adding user rep: ", err)
+		return database.User{}, err
+	}
+
+	return user, nil
+}
+
+func (repository *SqlUserRepository) CountUsers(ctx context.Context) (int64, error) {
+	count, err := repository.db.CountUsers(ctx)
+	if err != nil {
+		fmt.Println("error counting users rep: ", err)
+		return 0, err
+	}
+
+	return count, nil
 }
